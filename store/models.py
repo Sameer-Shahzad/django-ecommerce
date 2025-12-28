d0ff574b714e2cc6979528d00fc5d6cd520048fe
Sameer-Shahzad <sameershahzad835@gmail.com>
2025-12-29 01:45:40 +0500

    eleven commit

--- store/models.py
+++ store/models.py
@@ -18,7 +18,7 @@ class Product (models.Model):
     
     def get_url (self):
         return reverse ('product_detail', args = [self.category.slug, self.slug])
-    
+     
     
     def _str__(self):
         return self.product_name
